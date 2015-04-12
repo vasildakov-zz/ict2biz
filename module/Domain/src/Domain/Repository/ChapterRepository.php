649425645bfd6886d2de8474a87a9b2d1288deaa
Vasil Dakov <vasildakov@gmail.com>
2015-04-13 01:21:37 +0200

    Updated doctrine repositories, Domain model #2

---
+++ module/Domain/src/Domain/Repository/ChapterRepository.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * ChapterRepository
+ *
+ * @package Domain
+ * @subpackage Repository
+ * @author Vasil Dakov <vasildakov@gmail.com>
+ * @copyright Copyright (c) 2015 ict2biz (http://www.ict2biz.com/)
+ */
+
+namespace Domain\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class ChapterRepository extends \Doctrine\ORM\EntityRepository
+{
+
+}
---
+++ module/Domain/src/Domain/Repository/UserRepository.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * UserRepository
+ *
+ * @package Domain
+ * @subpackage Repository
+ * @author Vasil Dakov <vasildakov@gmail.com>
+ * @copyright Copyright (c) 2015 ict2biz (http://www.ict2biz.com/)
+ */
+
+namespace Domain\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class UserRepository extends EntityRepository
+{
+
+}
